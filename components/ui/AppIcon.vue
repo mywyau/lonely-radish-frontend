c5f219d5d1b1077969c30e59319e27d63b24eb71
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2026-05-07 13:49:44 +0100

    setup config

---
+++ components/ui/AppIcon.vue
@@ -0,0 +1,19 @@
+<!-- components/ui/AppIcon.vue -->
+<script setup lang="ts">
+import { appIcons, type AppIconName } from '@/config/ui/icons'
+
+const props = withDefaults(defineProps<{
+  icon: AppIconName
+  size?: string
+}>(), {
+  size: 'h-5 w-5',
+})
+</script>
+
+<template>
+  <Icon
+    :name="appIcons[props.icon]"
+    :class="props.size"
+    aria-hidden="true"
+  />
+</template>
